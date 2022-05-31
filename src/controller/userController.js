6ee5a997c4c4ef4d8645a58fc5b85f36bdf1233c
luuquangtri95 <luuquangtri95@gmail.com>
2022-05-31 17:07:30 +0700

    done nodejs init

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,9 +1,21 @@
 const userController = {
   getAllUser(req, res) {
+    // model => get all list user
+
     return res.render("user.ejs");
   },
   createNewUser(req, res) {
-    res.render("user.ejs");
+    // model => post data to database
+
+    return res.render("user.ejs");
+  },
+
+  deleteUser(req, res) {
+    return res.render("/user.ejs");
+  },
+
+  updateUser(req, res) {
+    return res.render("/user.ejs");
   },
 };
 
